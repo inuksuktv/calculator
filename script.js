0eb23eb63a434e5b8df4e28d137747d8a581c8f1
Bradley Morin <bradleymorin@gmail.com>
2024-10-11 00:26:53 -0600

    Clicking operator buttons displays result
    If a first number, operator, and second number have already been input,
    entering another operator causes the result to be displayed. The result
    is used as the first number of a new calculation.

--- script.js
+++ script.js
@@ -24,16 +24,16 @@ const subtractionButton = document.querySelector(".subtract");
 const additionButton = document.querySelector(".add");
 const equalsButton = document.querySelector(".equals");
 
-zeroButton.addEventListener("click", digit);
-oneButton.addEventListener("click", digit);
-twoButton.addEventListener("click", digit);
-threeButton.addEventListener("click", digit);
-fourButton.addEventListener("click", digit);
-fiveButton.addEventListener("click", digit);
-sixButton.addEventListener("click", digit);
-sevenButton.addEventListener("click", digit);
-eightButton.addEventListener("click", digit);
-nineButton.addEventListener("click", digit);
+zeroButton.addEventListener("click", digitInput);
+oneButton.addEventListener("click", digitInput);
+twoButton.addEventListener("click", digitInput);
+threeButton.addEventListener("click", digitInput);
+fourButton.addEventListener("click", digitInput);
+fiveButton.addEventListener("click", digitInput);
+sixButton.addEventListener("click", digitInput);
+sevenButton.addEventListener("click", digitInput);
+eightButton.addEventListener("click", digitInput);
+nineButton.addEventListener("click", digitInput);
 clearButton.addEventListener("click", clear);
 divisionButton.addEventListener("click", division);
 multiplicationButton.addEventListener("click", multiplication);
@@ -41,91 +41,112 @@ subtractionButton.addEventListener("click", subtraction);
 additionButton.addEventListener("click", addition);
 equalsButton.addEventListener("click", equals);
 
-function digit() {
-    if (displayIsStale) {
-        displayValue = this.textContent;
-        display.textContent = displayValue;
-        displayIsStale = false;
-    }
-    else if (displayValue.length < 9) {
-        displayValue += this.textContent;
-        display.textContent = displayValue;
-    }
+function digitInput() {
+  if (displayIsStale) {
+    displayValue = this.textContent;
+    display.textContent = this.textContent;
+    displayIsStale = false;
+  }
+  else if (displayValue.length < 9) {
+    displayValue += this.textContent;
+    display.textContent = displayValue;
+  }
 }
 
 function clear() {
-    firstNumber = "";
-    secondNumber = "";
-    currentOperator = null;
-    displayValue = "0";
-    display.textContent = displayValue;
-    displayIsStale = true;
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = null;
+  displayValue = "0";
+  display.textContent = displayValue;
+  displayIsStale = true;
 }
 
 function division() {
-    if (firstNumber === "") {
-        firstNumber = displayValue;
-        currentOperator = divide;
-        displayIsStale = true;
-    }
+  if (firstNumber === "") {
+    store(divide);
+  }
+  else if (currentOperator !== null) {
+    const result = calculateResult();
+    updateDisplay(result);
+  }
 }
 
 function multiplication() {
-    if (firstNumber === "") {
-        firstNumber = displayValue;
-        currentOperator = multiply;
-        displayIsStale = true;
-    }
+  if (firstNumber === "") {
+    store(multiply);
+  }
+  else if (currentOperator !== null) {
+    const result = calculateResult();
+    updateDisplay(result);
+  }
 }
 
 function subtraction() {
-    if (firstNumber === "") {
-        firstNumber = displayValue;
-        currentOperator = subtract;
-        displayIsStale = true;
-    }
+  if (firstNumber === "") {
+    store(subtract);
+  }
+  else if (currentOperator !== null) {
+    const result = calculateResult();
+    updateDisplay(result);
+  }
 }
 
 function addition() {
-    if (firstNumber === "") {
-        firstNumber = displayValue;
-        currentOperator = add;
-        displayIsStale = true;
-    }
+  if (firstNumber === "") {
+    store(add);
+  }
+  else if (currentOperator !== null) {
+    const result = calculateResult();
+    updateDisplay(result);
+  }
 }
 
 function equals() {
-    if (firstNumber !== "" && currentOperator !== null) {
-        secondNumber = displayValue;
-        const result = operate(firstNumber, secondNumber, currentOperator);
-        displayValue = result;
-        display.textContent = displayValue;
-        displayIsStale = true;
-    }
+  if (currentOperator !== null) {
+    const result = calculateResult();
+    updateDisplay(result);
+  }
 }
 
-function add(a, b) {
-    // This is concatening the inputs rather than performing addition.
-    return a + b;
+function store(operator) {
+  firstNumber = displayValue;
+  currentOperator = operator;
+  displayIsStale = true;
 }
 
-function subtract(a, b) {
-    return a - b;
+function calculateResult() {
+  return operate(firstNumber, displayValue, currentOperator);
 }
 
-function multiply(a, b) {
-    return a * b;
+function updateDisplay(result) {
+  displayValue = result;
+  firstNumber = result;
+  display.textContent = result;
+  displayIsStale = true;
 }
 
 function divide(a, b) {
-    if (b === 0) { throw new Error("Division by zero"); }
-    else return a / b;
+  if (b === 0) { throw new Error("Division by zero"); }
+  else return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
 }
 
 function operate(num1, num2, operation) {
-    return operation(num1, num2);
+  return operation(num1, num2);
 }
 
 module.exports = {
-    add, subtract, multiply, divide, operate
+  add, subtract, multiply, divide, operate
 }
